468c78e8fd73a170b401848604e669479c2e3541
John Jasa <johnjasa11@gmail.com>
2021-05-10 13:29:27 -0600

    Added IEA 15 MW

--- INN_interface/examples/01_run_WISDEM/blade_driver.py
+++ INN_interface/examples/01_run_WISDEM/blade_driver.py
@@ -5,11 +5,22 @@ from wisdem.commonse.mpi_tools import MPI
 from wisdem.postprocessing.compare_designs import run
 
 mydir = os.path.dirname(os.path.realpath(__file__))  # get path to this file
-fname_wt_input = mydir + os.sep + "BAR0.yaml"
+fname_wt_input = mydir + os.sep + "IEA-15-240-RWT.yaml"
 fname_modeling_options = mydir + os.sep + "modeling_options.yaml"
 fname_analysis_options = mydir + os.sep + "analysis_options.yaml"
 
 wt_opt, modeling_options, analysis_options = run_wisdem(fname_wt_input, fname_modeling_options, fname_analysis_options)
+
+cd = wt_opt['blade.interp_airfoils.cd_interp'][:, 110, 0, 0]
+cl = wt_opt['blade.interp_airfoils.cl_interp'][:, 110, 0, 0]
+r_thick = wt_opt['blade.interp_airfoils.r_thick_interp']
+
+
+print('cd', repr(cd))
+print('cl', repr(cl))
+print('L/D', repr(cl / cd))
+print('t/c', repr(r_thick))
+
 
 # if MPI:
 #     rank = MPI.COMM_WORLD.Get_rank()
