523618bdad899ac55c6d4df96ffb6cb7ec720ecf
John Jasa <johnjasa11@gmail.com>
2021-08-25 10:59:05 -0600

    Added 07 example

---
+++ INN_interface/examples/07_COBYLA_4_ld_opt/postprocess_opt.py
@@ -0,0 +1,55 @@
+"""
+Simple script to show how to grab all cases from a DOE run. User can then
+postprocess or plot further.
+"""
+
+import glob
+import os
+import sys
+import time
+
+import numpy as np
+
+import openmdao.api as om
+
+# Simply gather all of the sql files
+run_dir = os.path.dirname(os.path.realpath(__file__))
+output_path = os.path.join(run_dir, "outputs")
+optimization_logs = glob.glob(os.path.join(output_path, "*.sql*"))
+
+print(optimization_logs)
+
+all_data = {}
+for log in optimization_logs:
+    cr = om.CaseReader(log)
+    cases = cr.get_cases()
+    
+    for case in cases:
+        for key in case.outputs.keys():
+            if key not in all_data.keys():
+                all_data[key] = []
+            all_data[key].append(case.outputs[key])
+            
+            
+for key in all_data.keys():
+    all_data[key] = np.array(all_data[key])
+    
+import matplotlib.pyplot as plt
+
+l_d = 'inn_af.L_D_opt'
+cp = 'rotorse.rp.powercurve.Cp_regII'
+
+l_d = all_data[l_d][:, 2:]
+cp = all_data[cp]
+iters = range(len(cp))
+
+fig, ax = plt.subplots(2,1)
+ax[0].plot(iters, l_d)
+ax[1].plot(iters, cp)
+ax[0].set_xlabel('iteration')
+ax[1].set_xlabel('iteration')
+ax[0].set_ylabel('L/D')
+ax[1].set_ylabel('Cp')
+plt.tight_layout()
+
+plt.savefig('opt_history.png', dpi=300)
