a7352a827e02c0eb79c674f5266997b6d13f2860
John Jasa <johnjasa11@gmail.com>
2021-08-25 20:40:55 -0600

    Added more DVs case

--- INN_interface/examples/10_more_DVs/postprocess_opt.py
+++ INN_interface/examples/10_more_DVs/postprocess_opt.py
@@ -37,19 +37,27 @@ for key in all_data.keys():
 import matplotlib.pyplot as plt
 
 l_d = 'inn_af.L_D_opt'
+c_d = 'inn_af.c_d_opt'
+twist = 'blade.opt_var.twist_opt'
 cp = 'rotorse.rp.powercurve.Cp_regII'
 
 l_d = all_data[l_d][:, 2:]
+c_d = all_data[c_d][:, 2:]
+twist = all_data[twist][:, 2:]
 cp = all_data[cp]
 iters = range(len(cp))
 
-fig, ax = plt.subplots(2,1)
+fig, ax = plt.subplots(4, 1)
 ax[0].plot(iters, l_d)
-ax[1].plot(iters, cp)
+ax[1].plot(iters, c_d)
+ax[2].plot(iters, twist)
+ax[-1].plot(iters, cp)
 ax[0].set_xlabel('iteration')
-ax[1].set_xlabel('iteration')
+ax[-1].set_xlabel('iteration')
 ax[0].set_ylabel('L/D')
-ax[1].set_ylabel('Cp')
+ax[1].set_ylabel('cd')
+ax[2].set_ylabel('twist')
+ax[-1].set_ylabel('Cp')
 plt.tight_layout()
 
 plt.savefig('opt_history.png', dpi=300)
