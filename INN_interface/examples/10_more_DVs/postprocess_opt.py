8be1c33264f4f51d62f2deebbf2a5c2662f15c78
John Jasa <johnjasa11@gmail.com>
2021-08-26 09:09:20 -0600

    Cleaning up plot

--- INN_interface/examples/10_more_DVs/postprocess_opt.py
+++ INN_interface/examples/10_more_DVs/postprocess_opt.py
@@ -11,6 +11,7 @@ import time
 import numpy as np
 
 import openmdao.api as om
+import niceplots
 
 # Simply gather all of the sql files
 run_dir = os.path.dirname(os.path.realpath(__file__))
@@ -41,23 +42,41 @@ c_d = 'inn_af.c_d_opt'
 twist = 'blade.opt_var.twist_opt'
 cp = 'rotorse.rp.powercurve.Cp_regII'
 
-l_d = all_data[l_d][:, 2:]
-c_d = all_data[c_d][:, 2:]
-twist = all_data[twist][:, 2:]
-cp = all_data[cp]
+cutoff_idx = 150
+
+l_d = all_data[l_d][:cutoff_idx, 2:]
+c_d = all_data[c_d][:cutoff_idx, 2:]
+twist = all_data[twist][:cutoff_idx, 2:]
+cp = all_data[cp][:cutoff_idx]
 iters = range(len(cp))
+
+print(twist[0, :])
+
+labels = ['L/D', 'Cd', 'Twist, deg', 'Cp']
+ticks = np.arange(0, cutoff_idx/30 + 1) * 30
+yticks = [[60, 90, 120], [0.014, 0.017, 0.020], [-4, 2, 8], [.44, .45, .46]]
 
 fig, ax = plt.subplots(4, 1)
 ax[0].plot(iters, l_d)
 ax[1].plot(iters, c_d)
-ax[2].plot(iters, twist)
-ax[-1].plot(iters, cp)
-ax[0].set_xlabel('iteration')
-ax[-1].set_xlabel('iteration')
-ax[0].set_ylabel('L/D')
-ax[1].set_ylabel('cd')
-ax[2].set_ylabel('twist')
-ax[-1].set_ylabel('Cp')
+ax[2].plot(iters, np.rad2deg(twist))
+ax[-1].plot(iters, cp, color='k')
+ax[-1].set_xlabel('Optimization iteration')
+
+for idx, label in enumerate(labels):
+    ax[idx].set_ylabel(label, rotation=0, labelpad=30, ha='left')
+    ax[idx].set_xticks(ticks)
+    ax[idx].set_yticks(yticks[idx])
+    
+    
+fig.align_ylabels(ax)
+
+for subax in ax:
+    niceplots.adjust_spines(subax)
+
 plt.tight_layout()
+
+perc_cp = (cp[-1] - cp[0]) / cp[0] * 100.
+ax[-1].annotate(f'{perc_cp[0]:.2f}% increase in Cp', (.7, 0.5), fontsize=10, xycoords='axes fraction', annotation_clip=False)
 
 plt.savefig('opt_history.png', dpi=300)
