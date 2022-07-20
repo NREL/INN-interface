cfa7a3783480cb1beaf0a56e706041dad567e92e
Andrew Glaws <andrew.glaws@nrel.gov>
2022-07-20 11:47:37 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from io import open
 
 metadata = dict(
     name="INN_interface",
-    version="0.1.2",
+    version="0.1.3",
     description="Neural net and airfoil interface",
     author="Andrew Glaws",
     packages=["INN_interface"],
